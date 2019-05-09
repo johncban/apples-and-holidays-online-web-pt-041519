8090a0bf096a3df23663d3ab09db9856c9771671
johncban <johncban@gmail.com>
2019-05-09 20:14:20 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -66,9 +66,8 @@ def all_supplies_in_holidays(holiday_hash)
       puts" #{holiday}: #{supplies.join(" ")}"
     end
   end
+
 end
-puts "List of seasons and holidays supplies"
-all_supplies_in_holidays(holiday_hash)
 
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
